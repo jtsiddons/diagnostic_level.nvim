bbef5f68f0b506a475f62f646f4c03f89af7df95
jtsiddons <jtsiddons@gmail.com>
2024-03-16 17:49:59 +0000

    deprec: remove functionality to reassign prefixes.

--- README.md
+++ README.md
@@ -11,17 +11,6 @@ The functions that take `level` as input can take an integer between 1 and 4 (wh
 `set_diag_level(level)` - Display only messages of this `level`  
 `set_diag_min(level)` - Display only messages with severity >= this `level`
 
-#### Virtual Text Prefix
-
-`set_diag_prefixes({e = , w = , i = , h = })` - Prefixes for the diagnostic messages:
-
-* `e` : Error prefix. Default is `E: `
-* `w` : Warning prefix. Default is `W: `
-* `i` : Info prefix. Default is `I: `
-* `h` : Error prefix. Default is `H: `
-
-Note input is a table with keys, `e`, `w`, `i`, and `h`. Any can be included or omitted, defaults will be used when not set.
-
 ### Sign Column
 
 `set_sign_level(level)` - Add signs for only this `level`  
--- lua/diagnostic_level/init.lua
+++ lua/diagnostic_level/init.lua
@@ -45,32 +45,4 @@ M.set_underline_min = function(level)
     setdiag(level, true, 'underline')
 end
 
-M.set_diag_prefixes = function(t)
-    -- set defaults
-    setmetatable(t, { __index = { e = 'E: ', w = 'W: ', i = 'I: ', h = 'H: ' } })
-    local e = t[1] or t.e
-    local w = t[2] or t.w
-    local i = t[3] or t.i
-    local h = t[4] or t.h
-
-    local conf = get_diag_config()
-    local fmt = function(diagnostic)
-        if diagnostic.severity == vim.diagnostic.severity.ERROR then
-            return string.format("%s%s", e, diagnostic.message)
-        end
-        if diagnostic.severity == vim.diagnostic.severity.WARN then
-            return string.format("%s%s", w, diagnostic.message)
-        end
-        if diagnostic.severity == vim.diagnostic.severity.INFO then
-            return string.format("%s%s", i, diagnostic.message)
-        end
-        if diagnostic.severity == vim.diagnostic.severity.HINT then
-            return string.format("%s%s", h, diagnostic.message)
-        end
-        return diagnostic.message
-    end
-    conf.virtual_text.format = fmt
-    set_diag_config(conf)
-end
-
 return M
